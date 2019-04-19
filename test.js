2f57ee71e1453b299c41b67398b292ea8043a261
luolongzhi <luolongzhi@gmail.com>
2019-04-20 00:13:33 +0800

    add Makefile.audio

--- test.js
+++ test.js
@@ -5,8 +5,8 @@ ffmpeg({
   // Mount /data inside application to the current directory.
   mounts: [{type: "NODEFS", opts: {root: "."}, mountpoint: "/data"}],
   //arguments: ["-i", "/data/test.webm", "-c:v", "libvpx", "-an", "/data/out.webm"],
-  arguments: ["-i", "/data/pm.wav", "-f", "mp3", "/data/pm.mp3"],
-  //arguments: ["-i", "/data/pm.wav", "-c:a", "libfdk_aac", "-profile:a", "aac_he_v2", "-b:a", "24k", "-f", "adts", "/data/pm.aac"],
+  //arguments: ["-i", "/data/pm.wav", "-f", "mp3", "/data/pm.mp3"],
+  arguments: ["-i", "/data/pm.wav", "-c:a", "libfdk_aac", "-profile:a", "aac_he_v2", "-b:a", "24k", "-f", "adts", "/data/pm.aac"],
   //arguments: ["-i", "http://upos-hz-mirrorwcsu.acgvideo.com/upgcxcode/11/97/10959711/10959711-1-208.mp4?ua=tvproj&deadline=1543204884&gen=playurl&nbs=1&oi=2501663261&os=wcsu&platform=tvproj&trid=1b0c7d985e3244d0aa9f17a8979267bb&uipk=5&upsig=cf3afa552351bad66c2e9fef1f191a17&sparams=ua,deadline,gen,nbs,oi,os,platform,trid,uipk", "-f", "mp3", "/data/hehe.mp3"],
   //arguments: ["-i", "/data/1.mp4", "-f", "mp3", "/data/1.mp3"],
   stdin: function() {},
